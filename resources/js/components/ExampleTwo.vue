ea01c6e1f417f81e4fd9c4135e4b69fedcf0b0b2
Song Jae Hyeon <0nocsace@gmail.com>
2021-09-25 18:55:42 +0900

    example vue 2

---
+++ resources/js/components/ExampleTwo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container-fluid mt-5 justify-content-center">
+    <div class="row">
+      <div class="col mt-3">
+        <div class="card">
+          <div class="card-header">asd</div>
+          <div class="card-body">zxc</div>
+        </div>
+      </div>
+      <div class="w-100"></div>
+      <div class="col mt-3">
+        <div class="card">
+          <div class="card-header">asd</div>
+          <div class="card-body">456</div>
+        </div>
+      </div>
+    </div>
+    <div class="w-100"></div>
+    <div class="row">
+      <div class="col mt-3">
+        <div class="card">
+          <div class="card-header">asd</div>
+          <div class="card-body">zxc</div>
+        </div>
+      </div>
+      <div class="w-100"></div>
+      <div class="col mt-3">
+        <div class="card">
+          <div class="card-header">asd</div>
+          <div class="card-body">456</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
