3cb01407f48303f26dd0f9731cb56e4153bd479b
Song Jae Hyeon <0nocsace@gmail.com>
2021-09-28 15:56:59 +0900

    vue mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,3 +5,25 @@ mix.js("resources/js/app.js", "public/js")
     .postCss("resources/css/app.css", "public/css", [require("tailwindcss")]).sass('resources/sass/app.scss', 'public/css').browserSync({
         proxy: 'http://localhost:8000',
     });
+
+
+//.polyfill({
+//         enabled: true,
+//         useBuiltIns: "usage",
+//         targets: "firefox 50, IE 11"
+//     });
+
+
+// mix.js('resources/js/app.js', 'public/js')
+//     .vue()
+//     .postCss("resources/css/app.css", "public/css", [
+//         require("tailwindcss"),
+//     ])
+//     .browserSync({
+//         proxy: 'http://localhost:8000',
+//     })
+//     .polyfill({
+//         enabled: true,
+//         useBuiltIns: "usage",
+//         targets: "firefox 50, IE 11"
+//     });
