f494ec8ee9a6a96e0b1e9569d01f86a477b530aa
Song Jae Hyeon <0nocsace@gmail.com>
2021-09-29 21:59:49 +0900

    ex2

--- resources/js/components/ExampleTwo.vue
+++ resources/js/components/ExampleTwo.vue
@@ -3,15 +3,15 @@
     <div class="row">
       <div class="col mt-3">
         <div class="card">
-          <div class="card-header">asd</div>
+          <div class="card-header">asddddddd456546</div>
           <div class="card-body">zxc</div>
         </div>
       </div>
       <div class="w-100"></div>
       <div class="col mt-3">
         <div class="card">
-          <div class="card-header">asd</div>
-          <div class="card-body">456</div>
+          <div class="card-header">zz567asdasdasdasd</div>
+          <div class="card-body">456asd</div>
         </div>
       </div>
     </div>
@@ -19,15 +19,19 @@
     <div class="row">
       <div class="col mt-3">
         <div class="card">
-          <div class="card-header">asd</div>
-          <div class="card-body">zxc</div>
+          <div class="card-header">
+            <input type="text" placeholder="여기 입력" v-model="string" />
+          </div>
+          <div class="card-body">
+            <p>{{ string }}</p>
+          </div>
         </div>
       </div>
       <div class="w-100"></div>
       <div class="col mt-3">
         <div class="card">
-          <div class="card-header">asd</div>
-          <div class="card-body">456</div>
+          <div class="card-header">aasadsdasd</div>
+          <div v-if="show()" class="card-body">456123132345345</div>
         </div>
       </div>
     </div>
@@ -35,5 +39,22 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      string: "",
+    };
+  },
+  methods: {
+    // show: function () {
+    //   if (string.length > 5) {
+    //     return true;
+    //   }
+    //   //여기 지워야지 다시 화면나옴.
+    // },
+  },
+};
 </script>
+
+
+
